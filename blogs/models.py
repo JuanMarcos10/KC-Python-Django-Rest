aab73e446fa02e66fcecc1cfeadef478fa24779e
Juan Marcos <juanmarcosmarinperez@gmail.com>
2019-05-24 21:11:41 +0200

    02_Creamos nuestro modelo y super usuario

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+
+    IMPORTANT = 'IMP'
+    INFORMATION = 'INF'
+    NOTE = 'NOT'
+
+    LICENSES = [
+        [IMPORTANT, 'Important'],
+        [INFORMATION, 'Information'],
+        [NOTE, 'Note']
+    ]
+
+    title = models.CharField(max_length=150)
+    text = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=3, choices=LICENSES, default=INFORMATION)
