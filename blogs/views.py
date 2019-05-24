b3c4c78f1bbc95a0e2c27d0e8467cdbd19f7d140
Juan Marcos <juanmarcosmarinperez@gmail.com>
2019-05-24 23:34:37 +0200

    04_Primera Pagina web funcionando sin Posts

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blogs.models import Blog
+
+def index_blog(request):
+    # Recuperar las últimas fotos de la base de datos
+    blogs = Blog.objects.all()
+
+    # Creamos el contexto para pasarle las fotos a la plantilla
+    context = {'index_blog': blogs}
+
+    # Crear respuesta HTML con las fotos
+    html = render(request, 'index.html', context)
+
+    # Devolver la respuesta HTTP
+    return HttpResponse(html)
