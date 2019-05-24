5f0d9bed754138989dacb64f537f5f5dcff22654
Juan Marcos <juanmarcosmarinperez@gmail.com>
2019-05-24 23:49:11 +0200

    05_Modificacion del archivo admin.py para la administracion de los blogs-basico

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from blogs.models import Blog
 
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'text', 'creation_date')
+    list_filter = ('category',)
+    search_fields = ['title', 'description']
+
+
+admin.site.register(Blog, BlogAdmin)
